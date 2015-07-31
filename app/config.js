fa0f87980549e7f2a914e618dee9d4f4e9f394aa
Hack Reactor Students <accounts@hackreactor.com>
2015-07-30 19:41:21 -0700

    Moved to a Modulus.io database as Azure was not responding.

--- app/config.js
+++ app/config.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/my_database');
+if (process.env.NODE_ENV === 'production')
+  // Getting no response froM Azure MongoDB
+  mongoose.connect('mongodb://mmm:p0ptartz@apollo.modulusmongo.net:27017/ywu3dezI');
+else
+  mongoose.connect('mongodb://localhost/my_database');
 
 var db = mongoose.connection;
 
